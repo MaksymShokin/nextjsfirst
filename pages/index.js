8229aaf804713df954759600fbbc62638f6bae19
Maksym <maksym.sh@upplabs.com>
2021-03-05 22:27:38 +0200

    Link

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 const HomePage = () => {
   return (
     <div>
       <h1>This is home page</h1>
+      <Link href='portfolio'>Link to portfolio</Link>
+      <br/>
+      <Link replace href='clients'>
+        Link to clients
+      </Link>
     </div>
   );
 };

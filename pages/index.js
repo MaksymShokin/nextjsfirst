f58036739568819ff69b319ccaa7cf966c1f49b9
Maksym <maksym.sh@upplabs.com>
2021-03-05 22:35:19 +0200

    detailed-href

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,42 @@
 import Link from 'next/link';
 
 const HomePage = () => {
+  const clients = [
+    {
+      name: 'Max',
+      id: 'max'
+    },
+    {
+      name: 'Lol',
+      id: 'lol'
+    }
+  ];
+
   return (
     <div>
       <h1>This is home page</h1>
       <Link href='portfolio'>Link to portfolio</Link>
-      <br/>
+      <br />
       <Link replace href='clients'>
         Link to clients
       </Link>
+
+      {clients.map(({ name, id }) => (
+        <>
+          <br />
+          <Link
+            href={{
+              pathname: 'clients/[id]',
+              query: {
+                id: id
+              }
+            }}
+          >
+            {name}
+          </Link>
+          <br />
+        </>
+      ))}
     </div>
   );
 };

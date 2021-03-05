47c6d9ecf6f9f18f7cbe1d374cf59de0f0784091
Maksym <maksym.sh@upplabs.com>
2021-03-05 21:24:43 +0200

    basic-routing

---
+++ pages/portfolio/[id].js
@@ -0,0 +1,11 @@
+import {useLocation} from 'r'
+
+const PortfolioPageWith = () => {
+  return (
+    <div>
+      <h1>This is PortfolioPageWith</h1>
+    </div>
+  );
+};
+
+export default PortfolioPageWith;
---
+++ pages/portfolio/index.js
@@ -0,0 +1,9 @@
+const PortfolioUsPage = () => {
+  return (
+    <div>
+      <h1>This is PortfolioUsPage</h1>
+    </div>
+  );
+};
+
+export default PortfolioUsPage;
